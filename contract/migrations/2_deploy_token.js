7f34c7fe23145af968d70d07926b586290f5d11a
mihnhyuk <39216406+mihnhyuk@users.noreply.github.com>
2023-01-18 02:33:40 +0900

    Revert "Liquidity contract test"

--- contract/migrations/2_deploy_token.js
+++ contract/migrations/2_deploy_token.js
@@ -1,15 +1,14 @@
 const token = artifacts.require('WEMEXToken');
-const dummy1 = artifacts.require('dummyToken1');
-const dummy2 = artifacts.require('dummyToken2');
-const liquidity = artifacts.require('Liquidity');
+const exchange = artifacts.require('Liquidity');
 const _name = 'WEMEXToken';
 const _symbol = 'MEX';
+const _decimals = 18;
+const web3 = require('web3');
 const _total_supply = web3.utils.toWei('100000', 'ether');
 
-module.exports = async function (deployer) {
-
-  await deployer.deploy(dummy1, "token1", "tk1", _total_supply)
-  await deployer.deploy(dummy2, "token2", "tk2", _total_supply)
-  await deployer.deploy(liquidity, dummy1.address, dummy2.address);
-  
+module.exports = function (deployer) {
+  // deployer.deploy(token, _name, _symbol, _total_supply);
+  deployer.deploy(token, _name, _symbol, _total_supply).then(() => {
+    return deployer.deploy(exchange, token.address);
+  });
 };
