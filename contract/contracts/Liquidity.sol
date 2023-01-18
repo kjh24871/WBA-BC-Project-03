0bdd3cfa1af84f3e86d8b942e27767409c007f6d
mihnhyuk <mihnhyuk@naver.com>
2023-01-18 19:20:54 +0900

    백앤드 초기 세팅

--- contract/contracts/Liquidity.sol
+++ contract/contracts/Liquidity.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Liquidity is ERC20{
   IERC20 tokenA;
