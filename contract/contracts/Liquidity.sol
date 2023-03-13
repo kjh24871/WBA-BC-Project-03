a8fb67aedd7f8d8248184c65fe306f27df2515c8
koJungHoon <kjh24871@naver.com>
2023-03-13 15:17:33 +0900

    [FEAT] 리워드 분배 방식 변경

--- contract/contracts/Liquidity.sol
+++ contract/contracts/Liquidity.sol
@@ -7,15 +7,15 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Liquidity is ERC20{
   IERC20 tokenA;
   IERC20 tokenB;
-  uint256 fee = 0;
-  address payable[] delegators;
+  uint256 fee;
+  mapping(address => uint256) delegators;
 
-  event Swap(address caller, address input, address output, uint256 inputAmount, uint256 outputAmount);
-  event AddLiquidity(address caller, uint256 amount);
-  event RemoveLiquidity(address caller, uint256 amount);
+  event Swap(address indexed caller, address input, address output, uint256 inputAmount, uint256 outputAmount);
+  event AddLiquidity(address indexed caller, uint256 amount);
+  event RemoveLiquidity(address indexed caller, uint256 amount);
   constructor (
     address _tokenA, 
-    address _tokenB, 
+    address _tokenB,
     string memory name, 
     string memory sym
     ) ERC20(name, sym){
@@ -25,13 +25,14 @@ contract Liquidity is ERC20{
 
   function addLiquidity(uint256 _desiredAmountA, uint256 _desiredAmountB) public
   returns(uint256 actualAmountA, uint256 actualAmountB){
+    //공급량이 0이면 liquidity의 LP비율을 처음 공급량으로 설정
     if (totalSupply() == 0){
       actualAmountA = _desiredAmountA;
       actualAmountB = _desiredAmountB;
     } else{
       uint256 amountA = tokenA.balanceOf(address(this));
       uint256 amountB = tokenB.balanceOf(address(this));
-      
+      //이후로는 이전의 LP비율 만큼만 Pool에 집어 넣기 -> actualAmount = 비율 조정된 토큰 양
       actualAmountA = _desiredAmountB * amountA / amountB;
       if (actualAmountA <= _desiredAmountA){
         actualAmountB = _desiredAmountB;
@@ -44,7 +45,7 @@ contract Liquidity is ERC20{
     tokenB.transferFrom(msg.sender, address(this), actualAmountB);
     _mint(msg.sender, actualAmountB);
     if (!isExists(msg.sender)){
-      delegators.push(payable(msg.sender));
+      delegators(payable(msg.sender));
     }
     emit AddLiquidity(msg.sender, actualAmountB);
     return (actualAmountA, actualAmountB);
@@ -76,14 +77,14 @@ contract Liquidity is ERC20{
     return outputAmount;
   }
 
-  function pullRewardWithServer() public{
-    uint256 amount;
-    uint256 totalSupply = totalSupply();
-    for(uint256 i = 0 ; i < delegators.length ; i++){
-      amount = address(this).balance * balanceOf(delegators[i]) / totalSupply;
-      delegators[i].transfer(amount);
-    }
-  }
+  // function pullRewardWithServer() public{
+  //   uint256 amount;
+  //   uint256 totalSupply = totalSupply();
+  //   for(uint256 i = 0 ; i < delegators.length ; i++){
+  //     amount = address(this).balance * balanceOf(delegators[i]) / totalSupply;
+  //     delegators[i].transfer(amount);
+  //   }
+  // }
 
   function pullRewardWithClient() public{
     uint256 myRatio;
@@ -94,6 +95,10 @@ contract Liquidity is ERC20{
     
     // Todo : PullReward 방식 수정
     msg.sender.transfer(amount);
+  }
+
+  function changeFee(uint256 newFee) public view{
+    fee = newFee;
   }
 
   function getSwapRatio(uint256 _inputAmount, uint256 _liquidityInput, uint256 _liquidityOutput)
@@ -108,12 +113,10 @@ contract Liquidity is ERC20{
   }
 
   function isExists(address user) private view returns (bool) {
-    for (uint i = 0; i < delegators.length; i++) {
-        if (delegators[i] == user) {
-            return true;
-        }
+    if (delegators[user] = 0){
+      return false;
     }
-    return false;
+    return true;
   }
   function getLiquidity() public view returns (uint256 liquidity, uint256 amountA, uint256 amountB){
     liquidity = totalSupply();
@@ -124,5 +127,9 @@ contract Liquidity is ERC20{
 
   function getTokenAddress() public view returns (address, address){
     return (address(tokenA), address(tokenB));
+  }
+
+  function getPoolAddress() public view returns (address){
+    return (address(this));
   }
 }
